b9200c5cff70951400a07ec6a0a69b5b8dbeef9b
cihatislamdede <chtddgs@hotmail.com>
2022-04-22 02:11:20 +0300

    add share photo and tags section

--- hipogram/posts/urls.py
+++ hipogram/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import post_list_view
+from .views import post_create_view, post_list_view
 
 app_name = "posts"
 
 urlpatterns = [
     path("", post_list_view, name="list"),
+    path("create/", post_create_view, name="create"),
 ]
--- hipogram/posts/views.py
+++ hipogram/posts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
-from .models import Post
-
+from hipogram.posts.models import Post
 
 def post_list_view(request):
     posts = Post.objects.all().order_by("-creation_datetime")
     return render(request, "post_list.html", {'posts': posts})
+
+#Create post creation page
+def post_create_view(request):
+    return render(request, 'post_create.html')
