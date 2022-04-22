eb7a6c010e489bb6bfdaea89055f9d696bfc4ff5
cihatislamdede <chtddgs@hotmail.com>
2022-04-22 16:27:44 +0300

    add pagination and change tags

--- hipogram/posts/views.py
+++ hipogram/posts/views.py
@@ -1,10 +1,27 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from hipogram.posts.forms import PostForm
 from hipogram.posts.models import Post
+from django.core.paginator import Paginator
 
 def post_list_view(request):
     posts = Post.objects.all().order_by("-creation_datetime")
-    return render(request, "post_list.html", {'posts': posts})
+    paginator = Paginator(posts, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, "post_list.html", {'posts': page_obj})
+    
 
 #Create post creation page
 def post_create_view(request):
-    return render(request, 'post_create.html')
+    if request.user.is_authenticated:
+        form = PostForm()
+        if request.method == 'POST': 
+            form = PostForm(request.POST, request.FILES)
+            if form.is_valid():
+                new_post = form.save(commit=False)
+                new_post.created_by = request.user
+                new_post.save()
+                form.save_m2m()
+                return HttpResponseRedirect('/')
+        return render(request, 'post_create.html', context={'form': form})
